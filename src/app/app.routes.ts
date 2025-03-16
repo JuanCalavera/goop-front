a39d6f73f3695188ddea97f2a81c16f3fcfe59a2
Juan Calavera <juanconecti@gmail.com>
2025-03-16 04:54:38 -0300

    Put pedidos in route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PedidosComponent } from './pages/pedidos/pedidos.component';
+import { ItemsComponent } from './pages/items/items.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: PedidosComponent
+    },
+    {
+        path: "itens",
+        component: ItemsComponent
+    }
+];
